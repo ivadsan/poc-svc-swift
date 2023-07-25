932f6954bef9773ce47f0d2401f279b258730cc6
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2023-07-25 18:10:59 -0500

    Initial Commit

---
+++ SwiftSVC/SwiftSVCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSVCApp.swift
+//  SwiftSVC
+//
+//  Created by Iván Darío Sánchez Jiménez on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSVCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
